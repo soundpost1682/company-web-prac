be381632d033711a84c92daea1b0aa3eec3ba17f
evan <guarneri1682@gmail.com>
2025-05-30 12:14:10 -0700

    BE MongoDB, user Schema

--- back/index.js
+++ back/index.js
@@ -1,11 +1,18 @@
-const express = require('express')
-const app = express()
-const PORT = 3000
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const PORT = 3000;
 
-app.get('/', (req, res) =>{
-  res.send('hello world')
-})
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
 
-app.listen(PORT, ()=>{
-  console.log('server is running !')
-})
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("connection success!"))
+  .catch((error) => console.log("mongoDB failed!", error));
+
+app.listen(PORT, () => {
+  console.log("server is running !");
+});
