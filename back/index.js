0240833963b0f5c2a80f437e0801305176dc3a19
evan <guarneri1682@gmail.com>
2025-05-31 11:06:54 -0700

    BE signout fixing

--- back/index.js
+++ back/index.js
@@ -1,12 +1,14 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const cookieParser = require('cookie-parser')
 const app = express();
 const PORT = 3000;
 
 const userRoutes = require('./routes/user')
 app.use(express.json())
 app.use(express.urlencoded())
+app.use(cookieParser())
 
 app.use('/api/auth', userRoutes)
 
--- back/routes/user.js
+++ back/routes/user.js
@@ -73,6 +73,9 @@ router.post("/login", async (req, res) => {
       process.env.JWT_SECRET,
       { expiresIn: "24h" }
     );
+
+    console.log(token)
+
     res.cookie('token', token,{
       httpOnly : true,
       secure: 'production',
@@ -88,4 +91,32 @@ router.post("/login", async (req, res) => {
     res.status(500).json({ message: "server error!" });
   }
 });
+
+router.post('/logout', async (req, res)=>{
+  try{
+    const token = req.cookies.token
+    if (!token){
+      return res.status(400).json({message: 'already sign out'})
+    }
+    try{
+      const decoded = jwt.verify(token, process.env.JWT_SECRET)
+      const user = await User.findById(decoded.userID)
+      if (user){
+        user.isLoggedIn = false
+        await user.save()
+      }
+    }catch(error){
+      console.log('token verify error : ', error.message)
+    }
+    res.clearCookie('token', {
+      httpOnly : true,
+      secure : false,
+      sameSite: 'strict'
+    })
+    res.json({message : 'logged out done'})
+  }catch(error){
+    console.log('loggout error : ', error.message)
+    res.status(500).json({message : 'server error'})
+  }
+})
 module.exports = router;
